98e60051058e25e9383226e86785e2fa2079eda0
protyushabarua09 <76575009+protyushabarua09@users.noreply.github.com>
2021-01-02 23:29:39 +0600

    Delete language_function.php

--- setting/language_function.php
+++
@@ -1,6 +0,0 @@
- function deleteYear(Id){
-      var iAnswer = confirm("Are you sure you want to delete this Year ?");
-    if(iAnswer){
-        window.location = '<?php echo WEB_URL;?>setting/year_setup.php?delid=' + Id;
-    }
-  }
