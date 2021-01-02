7858e5f7791121c6839fbdc2667b5f30fd76a700
protyushabarua09 <76575009+protyushabarua09@users.noreply.github.com>
2021-01-02 23:25:21 +0600

    Delete employeelist_function.php

--- employee/employeelist_function.php
+++
@@ -1,6 +0,0 @@
-function deleteEmployee(Id){
-      var iAnswer = confirm("Are you sure you want to delete this Employee ?");
-    if(iAnswer){
-        window.location = '<?php echo WEB_URL; ?>employee/employeelist.php?id=' + Id;
-    }
-  }
