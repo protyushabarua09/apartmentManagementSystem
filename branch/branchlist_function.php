8c376a83ab1bc668746855427da4de1153597244
protyushabarua09 <76575009+protyushabarua09@users.noreply.github.com>
2021-01-02 23:23:42 +0600

    Delete branchlist_function.php

--- branch/branchlist_function.php
+++
@@ -1,6 +0,0 @@
-function deleteBranch(Id){
-      var iAnswer = confirm("Are you sure you want to delete this branch ?");
-    if(iAnswer){
-        window.location = 'branchlist.php?id=' + Id;
-    }
-  }
