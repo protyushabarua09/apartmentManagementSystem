30ef496ece22f4494b18a0f210ed047c008c5028
protyushabarua09 <76575009+protyushabarua09@users.noreply.github.com>
2021-01-02 23:28:13 +0600

    Delete ownerlist_function.php

--- owner/ownerlist_function.php
+++
@@ -1,6 +0,0 @@
-function deleteOwner(Id){
-      var iAnswer = confirm("Are you sure you want to delete this Owner ?");
-    if(iAnswer){
-        window.location = '<?php echo WEB_URL; ?>owner/ownerlist.php?id=' + Id;
-    }
-  }
