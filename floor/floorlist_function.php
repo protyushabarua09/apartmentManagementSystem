348029bec68a385bf905e08156c72fbdaf3847a6
protyushabarua09 <76575009+protyushabarua09@users.noreply.github.com>
2021-01-02 23:26:29 +0600

    Delete floorlist_function.php

--- floor/floorlist_function.php
+++
@@ -1,6 +0,0 @@
-function deleteFloor(Id){
-      var iAnswer = confirm("Are you sure you want to delete this Floor ?");
-    if(iAnswer){
-        window.location = '<?php echo WEB_URL; ?>floor/floorlist.php?id=' + Id;
-    }
-  }
