d787eeb18e6563837b762f12b7e5781fa168781f
protyushabarua09 <76575009+protyushabarua09@users.noreply.github.com>
2021-01-02 23:25:54 +0600

    Delete fairlist_function.php

--- fair/fairlist_function.php
+++
@@ -1,6 +0,0 @@
-function deleteFair(Id){
-      var iAnswer = confirm("Are you sure you want to delete this Fair ?");
-    if(iAnswer){
-        window.location = '<?php echo WEB_URL; ?>fair/fairlist.php?id=' + Id;
-    }
-  }
