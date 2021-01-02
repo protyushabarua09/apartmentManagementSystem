3c58e9fd7df33cdb037d1273a5f56066dc024391
protyushabarua09 <76575009+protyushabarua09@users.noreply.github.com>
2021-01-02 23:28:43 +0600

    Delete rentlist_function.php

--- rent/rentlist_function.php
+++
@@ -1,6 +0,0 @@
-function deleteRent(Id){
-      var iAnswer = confirm("Are you sure you want to delete this Rent ?");
-    if(iAnswer){
-        window.location = '<?php echo WEB_URL; ?>rent/rentlist.php?id=' + Id;
-    }
-  }
