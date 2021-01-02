b3c9d16c310ad096ec8ea7e76651cb89d086f89d
protyushabarua09 <76575009+protyushabarua09@users.noreply.github.com>
2021-01-02 23:29:23 +0600

    Delete admin_function.php

--- setting/admin_function.php
+++
@@ -1,6 +0,0 @@
-function deleteAdmin(Id){
-      var iAnswer = confirm("Are you sure you want to delete this Admin ?");
-    if(iAnswer){
-        window.location = '<?php echo WEB_URL;?>setting/admin.php?delid=' + Id;
-    }
-  }
