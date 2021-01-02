b625becb3b6b10a73c8460b2295f4b77e315f8b1
protyushabarua09 <76575009+protyushabarua09@users.noreply.github.com>
2021-01-02 23:24:40 +0600

    Delete complainlist_functions.php

--- complain/complainlist_functions.php
+++
@@ -1,6 +0,0 @@
-function deleteComplain(Id){
-      var iAnswer = confirm("Are you sure you want to delete this Complain ?");
-    if(iAnswer){
-        window.location = '<?php echo WEB_URL; ?>complain/complainlist.php?id=' + Id;
-    }
-  }
